498c336adc6354888ca47c5f1f5a58e1c5fd0745
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-10-14 21:15:49 +0530

    daily Update

--- _06Array/array.java
+++ _06Array/array.java
@@ -2,7 +2,9 @@ package _06Array;
 
 public class array {
         public static void main(String[] args) {
-                System.out.println("Array");
-                System.out.println("Array");
+                int[] arr={10,20,30,40,50};
+                System.out.println(arr[3]);// accessing
+                arr[3]=90;
+                System.out.println(arr[3]);//updating
         }
 }
