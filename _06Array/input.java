3b3b6ffd6c2d3f018a819b2bedc768741e05ff52
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-10-15 09:20:33 +0530

    daily Update

---
+++ _06Array/input.java
@@ -0,0 +1,21 @@
+package _06Array;
+
+import java.util.Scanner;
+
+public class input {
+        public static void main(String[] args) {
+                @SuppressWarnings("resource")
+                Scanner sc=new Scanner(System.in);
+                System.out.print("Enter The size of Array: ");
+                int n=sc.nextInt();
+                System.out.print("Enter the elements ");
+                int[] arr=new int[n];
+                for(int i=0;i<n;i++){
+                        arr[i]=sc.nextInt();
+                }
+                System.out.print("The elements in the array are ");
+                for(int i=0;i<n;i++){
+                        System.out.print(arr[i]+" ");
+                }
+        }
+}
---
+++ _06Array/inputOutput.java
@@ -0,0 +1,11 @@
+package _06Array;
+
+public class inputOutput {
+        public static void main(String[] args) {
+                int[] arr={1,3,5,6,7};
+                int n=arr.length;
+                for(int i=0;i<n;i++){
+                        System.out.print(arr[i]+" ");
+                }
+        }
+}
