b41d3a5346c8888adbea740fd8f8d0cdfecb721c
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-10-24 20:04:50 +0530

    daily Update

---
+++ _7_2DArray/searchIn2darray.java
@@ -0,0 +1,7 @@
+package _7_2DArray;
+
+public class searchIn2darray {
+        public static void main(String[] args) {
+                System.out.println("nahi ho pa raha hai yarr...");
+        }
+}
