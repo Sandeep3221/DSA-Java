232406bdc5ff647cca7cc30368e2bb1be7cd72c5
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-10-30 12:07:21 +0530

    Daily Update

---
+++ _8Strings/comparetoStringMethod.java
@@ -0,0 +1,7 @@
+public class comparetoStringMethod {
+        public static void main(String[] args) {
+                String str="sandeep";
+                String str1="anamika";
+                System.out.println(str.compareTo(str1));
+        }
+}
