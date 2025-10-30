a686a4deb41874b94a4b012e11e7b7429154062a
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-10-30 12:27:16 +0530

    Daily Update

---
+++ _8Strings/concatination.java
@@ -0,0 +1,7 @@
+public class concatination {
+        public static void main(String[] args) {
+                String s="Sandeep";
+                String ss="Adhikari";
+                System.out.println(s.concat(ss));
+        }
+}
