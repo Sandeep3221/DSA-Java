a68dfe533f14562cf1bd58b5c914f23864b87b0b
Sandeep3221 <kashyapadhikari09@gmail.com>
2025-11-05 09:48:32 +0530

    Daily Update

---
+++ _8Strings/stringToCharacterArray.java
@@ -0,0 +1,9 @@
+public class stringToCharacterArray {
+        public static void main(String[] args) {
+                String s="sandeep";
+                char arr[]=s.toCharArray();
+                for(char ele:arr){
+                        System.out.println(ele);
+                }
+        }
+}
---
+++ _8Strings/subString.java
@@ -0,0 +1,6 @@
+public class subString {
+        public static void main(String[] args) {
+                String s="sandeep";
+                System.out.println(s.substring(2,s.length()-1));
+        }
+}
